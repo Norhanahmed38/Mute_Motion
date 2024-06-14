fd0e797e8c44bd5d5aa5f8bbb6f918d5927a8742
Norhanahmed38 <na0703346@gmail.com>
2024-06-14 17:48:14 +0300

    update

--- lib/core/utils/models/place_model.dart
+++ lib/core/utils/models/place_model.dart
@@ -4,16 +4,20 @@ class PlaceModel {
   final int id;
   final String name;
   final LatLng latLng;
-  PlaceModel(
-  {
+  PlaceModel({
     required this.name,
     required this.id,
     required this.latLng,
-});
+  });
 }
 
-  List<PlaceModel> places = [
-    PlaceModel(id: 1,name: 'friends cafe',latLng: const LatLng(30.020148604297287, 31.003604883802165)),
-    PlaceModel(id: 2,name: 'Ripple',latLng: const LatLng(30.020114372031475, 31.00348251035441)),
-
-  ];
+List<PlaceModel> places = [
+  PlaceModel(
+      id: 1,
+      name: 'friends cafe',
+      latLng: const LatLng(30.020148604297287, 31.003604883802165)),
+  PlaceModel(
+      id: 2,
+      name: 'Ripple',
+      latLng: const LatLng(30.020114372031475, 31.00348251035441)),
+];
