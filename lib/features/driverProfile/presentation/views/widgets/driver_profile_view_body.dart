64ea755c8641a971c20bcf17e355e462f9911aef
Norhanahmed38 <na0703346@gmail.com>
2024-07-04 18:23:53 +0300

    .

--- lib/features/driverProfile/presentation/views/widgets/driver_profile_view_body.dart
+++ lib/features/driverProfile/presentation/views/widgets/driver_profile_view_body.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:mute_motion_passenger/constants.dart';
 import 'package:mute_motion_passenger/features/Rating/presentation/views/rating_view.dart';
 import 'package:mute_motion_passenger/features/chat/presentation/views/chat_screen_view.dart';
+import 'package:mute_motion_passenger/features/chat/presentation/views/widgets/chat_screen_view_body.dart';
 import 'package:mute_motion_passenger/features/driverProfile/presentation/views/widgets/customtext.dart';
 import 'package:mute_motion_passenger/features/requests/data/models/driver_model.dart';
 import 'package:mute_motion_passenger/features/driverProfile/data/request_api.dart';
@@ -248,7 +249,7 @@ class _DriverProfileViewBodyState extends State<DriverProfileViewBody> {
                                     style: TextStyle(
                                         fontSize: 20.sp, color: Colors.white)),
                                 onPressed: () {
-                                  navigateTo(context, RatingViewScreen());
+                                  navigateTo(context, ChatScreenView());
                                 },
                                 style: ElevatedButton.styleFrom(
                                   shape: RoundedRectangleBorder(
